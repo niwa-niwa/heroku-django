4227707c184f2d355bfd2d49ba0e1e2ad1037df2
niwa-niwa <urban.hang274@gmail.com>
2021-05-23 10:51:34 +0900

    added required files for heroku

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m+hfhobd-&__763pmqvotwi%2dw5@t-ulhj4y7!a%!=var8h34'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hecker-app.herokuapp.com', '127.0.0.1', 'localhost' ]
 
 
 # Application definition
